4bc22bc8fe3fcf4fceff8e496cfd7dfb3c7ddf5b
cheezyguy <me.abhinavyadav2@gmail.com>
2023-07-16 21:23:31 +0530

    Recommit Initial changes

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'YOUR_GMAIL_ADDRESS',
+      pass: 'YOUR_GMAIL_PASSWORD'
+    }
+  },
+  dbConnstring: 'mongodb://127.0.0.1:27017/Collabplatform',
+  sessionKey: 'HaloCode4Share'
+}
